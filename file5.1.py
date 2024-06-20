81161d18a8691f338d8af3d7e494037c131c70e9
Shikhar Kapoor <kapoorshikhar6@gmail.com>
2024-06-20 14:05:22 +0530

    ir

---
+++ file5.1.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+def newalarm():
+    pyautogui.hotkey("win","d")
+    pyautogui.hotkey("alt","space")
+    pyautogui.typewrite("clock")
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.click(778,411)
+    pyautogui.click(1156,410)
+    pyautogui.click(957,137)
+newalarm()
